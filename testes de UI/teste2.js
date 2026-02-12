7c9e07aa8cf72bddf96ad432011ec64c5032dfa1
Vieira16 <rafaelcevieira8@gmail.com>
2026-02-12 11:18:22 -0300

    afw

--- testes de UI/teste2.js
+++ testes de UI/teste2.js
@@ -3,11 +3,10 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
 const iniciarTeste = async () => {
     let driver = await new Builder().forBrowser(Browser.CHROME).build()
 
-    await driver.   get('https://www.google.com/')
+    await driver.get('https://http.cat')
 
     const inputs = await driver.findElements(By.css, ("textarea"));
-    await inputs[0].sendKeys("Senai");
-    await inputs[0].sendKeys(Key.RETURN);
+
 
 await driver.close();
 
